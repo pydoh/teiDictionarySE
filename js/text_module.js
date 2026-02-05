f1b4def11debd482df05be2130582dcc93441be4
kmetzker <pydoh@users.noreply.github.com>
2026-02-05 12:44:00 -0800

    Return uuid

--- js/text_module.js
+++ js/text_module.js
@@ -3,7 +3,8 @@ import { Snowflake } from "@theinternetfolks/snowflake";
 export function getSnowflake() {
   var uniqueid = Snowflake.generate({ timestamp: 1649157035498, shard_id: 4 })
   var uuid = '_' + uniqueid; // _6917065950617419944
-  console.log(uuid);
+//  console.log(uuid);
+  return uuid
 }
 
 function unEscape (entry_string) {
